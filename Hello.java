327fccda39f8eab9971c1435c5fc7fc4bac5aac6
[bayfaub] <[faubionbay@gmail.com]>
2021-03-10 19:03:19 -0800

    Changed Hello.java for part 3

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String [] args){
         System.out.println("Hello World!");
         System.out.println("The sky is full of stars.");
+        System.out.println("Project A for part 3 change");
     }
 }
