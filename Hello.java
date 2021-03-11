0b92ed322f5af9b93bbbe5df32f18a3c9f4d46ab
[bayfaub] <[faubionbay@gmail.com]>
2021-03-10 19:07:36 -0800

    A change for project B part 3 of the assignment.

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String [] args){
         System.out.println("Hello World!");
         System.out.println("The sky is full of stars.");
+        System.out.println("A change for Project B part 3 of the assignment");
     }
 }
